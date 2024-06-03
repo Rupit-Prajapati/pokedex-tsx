c21844130363d02eed32a6214c5c76970484c587
Roshni-Prajapati <rupit2330@gmail.com>
2024-06-03 21:05:42 +0530

    responsive modal design

--- src/components/PokemonListing/SinglePokemonList/PokemonDetail/PokemonPopup/PokemonModal.tsx
+++ src/components/PokemonListing/SinglePokemonList/PokemonDetail/PokemonPopup/PokemonModal.tsx
@@ -1,4 +1,4 @@
-import { Modal, ModalCloseButton, ModalContent, ModalOverlay } from '@chakra-ui/react'
+import { Modal, ModalCloseButton, ModalContent, ModalOverlay, Flex } from '@chakra-ui/react'
 import React, { ReactNode } from 'react'
 interface MyComponentProps {
   isOpen: boolean
@@ -10,7 +10,7 @@ const PokemonModal: React.FC<MyComponentProps> = ({ isOpen, onClose, children })
     <>
       <Modal isOpen={isOpen} onClose={onClose} isCentered>
         <ModalOverlay />
-        <ModalContent background={'linear-gradient(to bottom, rgb(43, 42, 42), rgb(27, 27, 27))'} color={"#FFF"} maxW={{ base: "750px" }} padding={10}>
+        <ModalContent background={'linear-gradient(to bottom, rgb(43, 42, 42), rgb(27, 27, 27))'} color={"#FFF"} maxW={{ base: "90%", sm: "450px", md: '700px' }} padding={{ base: 5, sm: 10 }}>
           <ModalCloseButton />
           {children}
         </ModalContent>
--- src/components/PokemonListing/SinglePokemonList/SingleList.tsx
+++ src/components/PokemonListing/SinglePokemonList/SingleList.tsx
@@ -67,17 +67,17 @@ const SingleList: React.FC<MyComponentsProps> = ({ singleUrl }) => {
           </Flex>
           <PokemonTypesDisplay types={singleData?.types || []} />
           <PokemonModal isOpen={isOpen} onClose={onClose} >
-            <Flex gap={5}>
-              <Flex direction={'column'} gap={2} height={'300px'} width={'300px'} background={'linear-gradient(to bottom,rgba(214, 214, 214, 0.3),rgba(77, 77, 77, 0.1))'} boxShadow={'0 0 10px 10px rgba(202, 201, 201, 0.05)'} borderRadius={'50%'} justifyContent={'center'} alignItems={'center'}>
+            <Flex gap={5} flexWrap={'wrap'} justifyContent={'center'}>
+              <Flex direction={'column'} gap={2} height={{ base: "250px", sm: '300px' }} width={{ base: '250px', md: '300px' }} background={'linear-gradient(to bottom,rgba(214, 214, 214, 0.3),rgba(77, 77, 77, 0.1))'} boxShadow={'0 0 10px 10px rgba(202, 201, 201, 0.05)'} borderRadius={'50%'} justifyContent={'center'} alignItems={'center'}>
                 < Image
-                  maxW={'280px'}
-                  height={'280px'}
+                  maxW={{ base: "180px", sm: '280px' }}
+                  height={{ base: "180px", sm: '280px' }}
                   objectFit='contain'
                   src={singleData?.sprites.other.dream_world.front_default} alt={singleData?.name}
                 />
                 <PokemonTypesDisplay types={singleData?.types || []} />
               </Flex>
-              <Flex direction={'column'} gap={4} width={'350px'}>
+              <Flex direction={'column'} gap={4} width={{ base: '100%', md: '300px' }}>
                 <PokemonStat stats={singleData?.stats} />
               </Flex>
             </Flex>
