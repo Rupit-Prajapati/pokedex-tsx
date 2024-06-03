2cd699f4c2ed6d359364bd35f2a05c7133a57601
Roshni-Prajapati <rupit2330@gmail.com>
2024-06-03 21:20:47 +0530

    name added in the popup

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,6 @@ function App() {
     setLoading(true)
     setPokeUrls(null)
     const { data } = await axios.get<PokemonData>(url)
-    console.log(data)
     setNextUrl(data.next)
     setPokeUrls(data.results)
     setPrevUrl(data.previous)
